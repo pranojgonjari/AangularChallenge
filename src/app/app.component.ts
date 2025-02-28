43562927d1b289bed718d903d104f237ab184af8
pranojgonjari <gonjaripranoj@gmail.com>
2025-02-28 23:47:45 +0530

    One Way Data Binding, interpolation, Property Binding, Event Binding, Two way Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddEmployeeComponent } from './componant/add-employee/add-employee.component';
 import { EmployeeListComponent } from './componant/employee-list/employee-list.component';
+import { DataBindingComponent } from './componant/data-binding/data-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,AddEmployeeComponent,EmployeeListComponent],
+  imports: [RouterOutlet,DataBindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/componant/data-binding/data-binding.component.ts
+++ src/app/componant/data-binding/data-binding.component.ts
@@ -1,22 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.component.html',
   styleUrl: './data-binding.component.css'
 })
 export class DataBindingComponent {
 
   //string, number, boolean, date
-   coursename: string ="Angularb 18";
+   coursename: string ="Angular 18";
    inputType = "checkbox";
+   statename: string="";
+   inputType1 ="radio";
    rollno: number = 123;
+   myClassName: String = "bg-primary";
    isIndian: boolean = true;
    currentDate: Date = new Date(); 
 
+   firstName=  signal("Pranoj Gonjari");
+
    constructor(){
   
+   }
+
+   changeCourseName(){
+      this.coursename="React JS";
+      this.firstName.set("MS Dhoni..");
+   }
+
+   showAlert(message: string){
+    alert(message)
    }
 }
